4b5d229c616cbb548cbc2a1c3b6241e1613ebfd8
ExoduxNile <sandeep.tete2012@gmail.com>
2023-09-09 23:56:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
 
-    return config;
-  },
-};
+    return config
+  }
+}
 
 
 const nextConfig = {
